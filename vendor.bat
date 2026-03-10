0fe5ebd073473a8c25aac366a7f9e81078a90d43
NSimic <nsimic@gmail.com>
2026-03-11 00:17:16 +0100

    add clang format and vendoring utility

---
+++ vendor.bat
@@ -0,0 +1,3 @@
+xcopy /E /I /Y ..\fundamental\arch vendor\fundamental\arch
+xcopy /E /I /Y ..\fundamental\src vendor\fundamental\src
+xcopy /E /I /Y ..\fundamental\include vendor\fundamental\include
