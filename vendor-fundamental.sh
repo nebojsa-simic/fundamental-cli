ec4284333d6595d628d542b07d941a1f83ea55fd
Nebojsa Simic <nsimic@gmail.com>
2026-03-20 21:43:25 +0100

    Add vendor-fundamental.sh for Linux

---
+++ vendor-fundamental.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp -r ../fundamental/arch/. vendor/fundamental/arch/
+cp -r ../fundamental/src/. vendor/fundamental/src/
+cp -r ../fundamental/include/. vendor/fundamental/include/
+cp -r ../fundamental/.opencode/skills/. vendor/fundamental/.opencode/skills/
