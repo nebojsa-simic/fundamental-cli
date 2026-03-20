e77f1300876e9bf26f206e3bdadcc381749aceb8
Nebojsa Simic <nsimic@gmail.com>
2026-03-20 21:33:57 +0100

    Revert "Re-vendor fundamental after NULL define cleanup"
    This reverts commit 750cb293c005012d79102afaf55c56fa0268e239.

--- vendor/fundamental/arch/arch/async/linux-amd64/async.c
+++
@@ -1,9 +0,0 @@
-#include <time.h>
-
-unsigned long long arch_async_now_ms(void)
-{
-	struct timespec ts;
-	clock_gettime(CLOCK_MONOTONIC, &ts);
-	return (unsigned long long)ts.tv_sec * 1000ULL +
-		   (unsigned long long)ts.tv_nsec / 1000000ULL;
-}
--- vendor/fundamental/arch/arch/async/windows-amd64/async.c
+++
@@ -1,9 +0,0 @@
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#include <windows.h>
-
-unsigned long long arch_async_now_ms(void)
-{
-	return (unsigned long long)GetTickCount64();
-}
